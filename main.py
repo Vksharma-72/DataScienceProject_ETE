5a38f7edb14e1b8a1b88195ecdad26eba2b03aed
Vksharma-72 <kirshansharma3546@gmail.com>
2026-04-30 02:23:16 +0530

    The Model train is Updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.datascience.pipeline.model_train_pipelie import ModelTrainerTrainingPipeline
 
 logger.info("Welcome to our custom logging data science ")
 
@@ -37,3 +38,13 @@ if __name__ == "__main__":
     except Exception as e:
         raise e
     
+
+STAGE_NAME = "Model Trainer Stage "
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<<")
+        model_trainer = ModelTrainerTrainingPipeline()
+        model_trainer.initiate_model_training()
+        logger.info(f">>>>>>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<<<<\n\nx================x") 
+    except Exception as e:
+        raise e
