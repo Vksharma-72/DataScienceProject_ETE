4b65a51e747a19f58f5b1a4a14ca8df35ed3a9a3
Vksharma-72 <kirshansharma3546@gmail.com>
2026-04-30 01:12:16 +0530

    Transformation Module updated

--- src/datascience/pipeline/data_transformation_pipeline.py
+++ src/datascience/pipeline/data_transformation_pipeline.py
@@ -1,6 +1,7 @@
 from src.datascience.config.configration import ConfigurationManager
 from src.datascience.components.data_transformation import DataTransformation
 from src.datascience import logger
+from pathlib import Path
 
 STAGE_NAME = "Data Transformation Stage "
 
@@ -9,10 +10,19 @@ class DataTransformationPipeline:
         pass
 
     def initiate_data_transformation(self):
-        config=ConfigurationManager()
-        data_transformation_config = config.get_data_transfromation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_spliting()
+
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), 'r') as f:
+                status=f.read().split(" ")[-1]
+            if status=="True":
+                config=ConfigurationManager()
+                data_transformation_config = config.get_data_transfromation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+            else:
+                raise Exception("Your data schema is not valid")
+        except Exception as e:
+            print(e)
 
 if __name__ == "__name__":
     try:
